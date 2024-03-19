55fe03df2b1577f8c9a4d054f519442e42c6f96e
Finding-Paradise <janarplanet.kg@gmail.com>
2024-03-19 14:59:52 +0600

    Fix post request to add message

--- routers/messages.ts
+++ routers/messages.ts
@@ -3,21 +3,47 @@ import fs from 'fs';
 import { Message } from '../types';
 
 const messagesRouter = express.Router();
+const path = './messages';
+let messagesArr: Message[] = [];
 
-messagesRouter.get('/', (_req, res) => {
-  return res.send('[]');
+if (!fs.existsSync(path)) {
+  fs.mkdirSync(path);
+}
+
+const readFiles = async () => {
+  messagesArr = [];
+  const files = await fs.promises.readdir(path);
+  files.forEach((file) => {
+    const filePath = path + '/' + file;
+    const contents = fs.readFileSync(filePath, { encoding: 'utf-8' });
+    messagesArr.push(JSON.parse(contents));
+  });
+};
+
+readFiles().catch(console.error);
+
+messagesRouter.get('/', async (_req, res) => {
+  if (messagesArr.length < 5) {
+    return res.send(messagesArr);
+  }
+  const last5Messages = messagesArr.slice(
+    messagesArr.length - 5,
+    messagesArr.length
+  );
+  return res.send(last5Messages);
 });
 
-messagesRouter.post('/create', (req, res) => {
+messagesRouter.post('/create', async (req, res) => {
   const date = new Date().toISOString();
   const messageData: Message = {
     message: req.body.message,
     datetime: date,
   };
-  fs.writeFileSync(
-    `./messages/${'date'}.txt`,
+  await fs.promises.writeFile(
+    `./messages/${date}.txt`,
     JSON.stringify(messageData, null, 2)
   );
+  readFiles().catch(console.error);
   return res.send(messageData);
 });
 
